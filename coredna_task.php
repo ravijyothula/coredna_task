f9bd834a8890f98c3f85b3d9cfc8a54a42ff7327
Ravi Jyothula <ravi.jyothula@ausmeter.com.au>
2023-01-11 20:08:48 +1100

    Commit file

---
+++ coredna_task.php
@@ -0,0 +1,53 @@
+<?php
+
+class API {
+    function postData($url, $data)
+    {
+        $http_origin = "https://corednacom.corewebdna.com/assessment-endpoint.php";
+        $params = array(
+                'http' => array(
+                    'method' => 'POST',
+                    'header'=> 
+                        "Authorization: Bearer d417fcbc-94b7-4357-aff3-536c33364428\r\n"    
+                        ."content-Type: application/json; charset=utf-8;\r\n"
+                        ."Access-Control-Allow-Origin: $http_origin\r\n"
+                        ."Access-Control-Allow-Methods: GET, POST\r\n"
+                        ."Connection: close\r\n",
+                    'ignore_errors' => true,
+                    'content' => $data
+                ),
+                "ssl" => array(
+                    "ciphers" => "TLSv1",
+                )
+        );
+        $context = stream_context_create($params);
+        try {
+            $fp = @file_get_contents($url, false, $context);
+            if (!$fp) {
+                throw new Exception("failed to open stream ", 1);
+            }else{
+                echo "File is loaded and content is there";
+            }
+        }catch (Exception $e) {
+            echo $e->getMessage();
+        }
+        try{
+            $response = @stream_get_contents($fp);
+        }catch(Exception $e){
+            throw new Exception("Problem reading data from $url, $e->getMessage()");
+        }
+        
+        return $response;
+    }
+
+}
+
+$API = new API;
+$payload = array(
+    'name' => 'Ravi Jyothula',
+    'email' => 'ravijyothula@gmail.com',
+    'url' => 'https://github.com/ravijyothula/coredna_task.php'
+);
+
+echo $API->postData('https://corednacom.corewebdna.com/assessment-endpoint.php', json_encode($payload));
+?>
